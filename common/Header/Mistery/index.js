4fa9827c3b26abfe40d9758828310405c6c6bc26
Amaan Ahmad <amaanahmad@Amaans-MacBook-Air.local>
2023-08-10 16:08:16 +0530

    navbar changed

---
+++ common/Header/Mistery/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styles from "./styles.module.css";
+
+function Mistery() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.box}>X?</div>
+    </div>
+  );
+}
+
+export default Mistery;
--- common/Header/index.js
+++ common/Header/index.js
@@ -1,14 +1,14 @@
+import React from "react";
 import Navbar from "./Navbar";
 import SearchBar from "./SearchBar";
+import Umbrella from "./Mistery";
 import styles from "./styles.module.css";
 
 function Header() {
   return (
     <div className={styles.container}>
-      <div style={{ flex: 1 }}>..</div>
-
+      <Mistery /> //Todo: add something interested in this component
       <Navbar />
-
       <SearchBar />
     </div>
   );
